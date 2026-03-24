eea43fd94aae525d7c09ca3f0e7b113597b2f5dd
x.diaz <x.diaz@opendeusto.es>
2026-03-24 11:01:05 +0100

    Endpoints Coche

--- BackEnd/maven-basic-project/src/main/java/com/mycompany/app/CocheController.java
+++ BackEnd/maven-basic-project/src/main/java/com/mycompany/app/CocheController.java
@@ -3,6 +3,9 @@ package com.mycompany.app;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.List;
 
 @RestController
@@ -16,10 +19,21 @@ public class CocheController {
         this.cocheRepository = cocheRepository;
     }
 
-    // Cuando alguien visite la URL con el método GET, ejecutamos esto:
+    // Obtener todos los coches
     @GetMapping
     public List<Coche> obtenerTodosLosCoches() {
-
         return cocheRepository.findAll();
+    }
+
+    // Obtener coche por ID
+    @GetMapping("/{id}")
+    public Coche obtenerCocheID(@PathVariable Long id) {
+        return cocheRepository.findById(id).orElse(null);
+    }
+
+    // Obtener coches por marca
+    @GetMapping("/marca/{marca}")
+    public List<Coche> obtenerCochesMarca(@PathVariable String marca) {
+        return cocheRepository.findByMarca(marca);
     }
 }
--- BackEnd/maven-basic-project/src/main/java/com/mycompany/app/CocheRepository.java
+++ BackEnd/maven-basic-project/src/main/java/com/mycompany/app/CocheRepository.java
@@ -3,7 +3,11 @@ package com.mycompany.app;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CocheRepository extends JpaRepository<Coche, Long> {
-
+    // Consulta SQL se crea automáticamente:
+    // SELECT * FROM coche WHERE marca = ?
+    List<Coche> findByMarca(String marca);
 }

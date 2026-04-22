826fad802e602553df48d440c18b37f01365e102
x.diaz <x.diaz@opendeusto.es>
2026-04-22 23:16:11 +0200

    Final Fix Tests Unitarios

--- BackEnd/maven-basic-project/src/test/java/com/mycompany/app/security/JwtServiceTest.java
+++ BackEnd/maven-basic-project/src/test/java/com/mycompany/app/security/JwtServiceTest.java
@@ -73,9 +73,9 @@ public class JwtServiceTest {
     void testExpiredTokenIsInvalid() throws Exception {
         String expiredToken = generarTokenExpirado("test@correo.com");
 
-        boolean valido = jwtService.isTokenValid(expiredToken, userDetails);
-
-        assertFalse(valido);
+        assertThrows(io.jsonwebtoken.ExpiredJwtException.class, () -> {
+            jwtService.isTokenValid(expiredToken, userDetails);
+        }, "Debería lanzar ExpiredJwtException porque el token ha caducado");
     }
 
     private String generarTokenExpirado(String username) throws Exception {
